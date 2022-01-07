e58737ff57f4e5582ce44b6eba98820e9d597780
Usmanov Marat Ravshanovich <checlioschew@yandex.ru>
2022-01-07 13:38:13 +0300

    project with basic funcs added

---
+++ flash_chat/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flash_chat/screen/chat_screen.dart';
+import 'package:flash_chat/screen/login_screen.dart';
+import 'package:flash_chat/screen/registration_screen.dart';
+import 'package:flash_chat/screen/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      },
+    );
+  }
+}
